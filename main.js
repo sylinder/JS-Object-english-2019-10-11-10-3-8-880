acde00f8385187532da0486da2c3d4d32aa3f5ca
袁柱 <1522719885@qq.com>
2020-06-23 17:46:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+# 1
+var user = {};
+user.name = "John";
+user.surname = "Mike";
+console.log("user.name: " + user.name + "; user.surname: " + user.surname);
 
+user.name = "Peter";
+console.log("user.name: " + user.name);
+
+delete user.name;
+console.log(user.name);
+
+
+# 2
